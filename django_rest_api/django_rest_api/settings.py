d52dd7916cfca01fd7afe72c5f64f83624d0d6b0
Jean Balangue <jeanbalangue@hotmail.fr>
2023-11-16 17:22:18 +0100

    ajout authentication et 1ere migration

--- django_rest_api/django_rest_api/settings.py
+++ django_rest_api/django_rest_api/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
+    "authentication"
 ]
 
 MIDDLEWARE = [
@@ -116,3 +117,7 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+# moi
+AUTH_USER_MODEL = 'authentication.User'
