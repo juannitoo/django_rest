dca7b7bb3fb9478ebc57758816cc79d85d29bb18
Jean Balangue <jeanbalangue@hotmail.fr>
2023-11-19 15:27:05 +0100

    migration

--- django_rest_api/api/models.py
+++ django_rest_api/api/models.py
@@ -7,18 +7,17 @@ from mptt.models import MPTTModel, TreeForeignKey
 class Category(MPTTModel):
 
     #uuid pas bon pour la clarté !
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    # id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     date_created = models.DateTimeField(auto_now_add=True, verbose_name="Created date")
     date_updated = models.DateTimeField(auto_now=True, verbose_name="Updated date")
 
-    name = models.CharField(max_length=255, verbose_name="Name", null=False, unique=True)
+    name = models.CharField(max_length=255, null=False, blank=False, unique=True, verbose_name="Name")
+    slug = models.SlugField(blank=False, verbose_name="Slug")
     description = models.TextField(blank=True, verbose_name="Description")
-    slug = models.SlugField(default="", null=False, verbose_name="Slug")
 
     # Alors les trees je n'en avais jamais entendu parler, une sorte de graph ou d'index j'ai l'impression
     # j'ai donc pris rendez-vous avec la notice de django-mptt !
-    parent = TreeForeignKey("self", on_delete=models.CASCADE, null=True, blank=True, related_name="children")
-    #models.PROTECT ?
+    parent = TreeForeignKey("self", on_delete=models.PROTECT, null=True, blank=True, related_name="children")
 
     class Meta:
         ordering = ["name"]
@@ -32,17 +31,17 @@ class Category(MPTTModel):
 
 class Equipment(models.Model):
 
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    # id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     date_created = models.DateTimeField(auto_now_add=True, verbose_name="Created date")
     date_updated = models.DateTimeField(auto_now=True, verbose_name="Updated date")
 
     name = models.CharField(max_length=255, verbose_name="Name")
-    description = models.TextField(max_length=1000,blank=True, verbose_name="Description")
+    slug = models.SlugField(blank=False, verbose_name="Slug")
+    description = models.TextField(max_length=1000, blank=True, verbose_name="Description")
     quantity = models.PositiveBigIntegerField(null=False, default=0, verbose_name="Quantity")
-    slug = models.SlugField(default="", null=False, verbose_name="Slug")
 
     categories = models.ManyToManyField("Category", related_name="categories", verbose_name="categories")
-    # on_delete à gérér
+    #  on_delete=models.PROTECT, 
 
     class Meta:
         ordering = ["name"]
--- django_rest_api/api/serializers.py
+++ django_rest_api/api/serializers.py
@@ -7,7 +7,16 @@ class CategoryListSerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
         fields = ['id', 'name', 'description']
-    
+
+    def validate(self, data):
+        print('validation')
+        # Nous vérifions que la catégorie existe
+        if Category.objects.filter(name=data['name']).exists():
+        # En cas d'erreur, DRF nous met à disposition l'exception ValidationError
+            raise serializers.ValidationError('Category already exists errorirrfzf')
+        if len(data['slug']) == 0:
+            raise serializers.ValidationError('description vide')
+        return data    
 class CategoryDetailsSerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
